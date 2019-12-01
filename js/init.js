6c6ddb944a673b16b9da321be35f59f31c86a9f4
Anand <anandbaburajan@gmail.com>
2019-12-01 11:34:48 +0530

    Deleting files to update file structure

--- js/init.js
+++
@@ -1,5 +0,0 @@
-(function($) {
-  $(document).ready(function(){
-    $('.collapsible').collapsible();
-  });
-})(jQuery);
--- site.js
+++
@@ -1,3 +0,0 @@
-navigator.serviceWorker && navigator.serviceWorker.register('https://smashsdgs.me/sw.js').then(function(registration) {
-  console.log('Excellent, registered with scope: ', registration.scope);
-});
